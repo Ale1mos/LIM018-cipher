cacaddff4d29a6aff45fa65f860385b41faaf54e
Fiorella Mosqueira <fiorella.mosqueira.contractor@bbva.com>
2022-06-06 22:15:50 -0500

    minúsculas

--- src/cipher.js
+++ src/cipher.js
@@ -7,8 +7,24 @@ const cipher = {
       throw TypeError("No es letra")
     }
     
+    if(string === string.toUpperCase()){
+      var unirMayuscula = "";
     
-    var unir = "";
+    for(var j = 0;j<string.length;j++){
+      var formulamayus = (string.charCodeAt(j)-65+offset)%26+65;
+      var codificarmayus = String.fromCharCode(formulamayus);
+      unirMayuscula = unirMayuscula + codificarmayus;
+    }
+    
+    return unirMayuscula;
+      // console.log(string.toUpperCase())
+      // console.log(string.toLowerCase())
+    }
+    else{
+      // console.log(string)
+      string = string.toUpperCase();
+      // console.log(string)
+      var unir = "";
     
     for(var i = 0;i<string.length;i++){
       var a = string.charCodeAt(i);
@@ -17,7 +33,11 @@ const cipher = {
       unir = unir + codificar;
     }
     
-    return unir;
+    return unir.toLowerCase();
+
+    }
+    
+    
   },
 
     decode(offset,string){
@@ -28,7 +48,23 @@ const cipher = {
         throw TypeError("No es letra")
       }
     // offset = parseInt(offset);
-   
+   if(string === string.toLowerCase()){
+     string = string.toUpperCase()
+    var unirminus = "";
+    for(var j = 0;j<string.length;j++){
+      // var a = string.charCodeAt(j);
+      //console.log("a",a);
+      var formulaminus = (string.charCodeAt(j)+65-offset)%26+65;
+      //console.log("formula",formula)
+      
+      var codificarminus = String.fromCharCode(formulaminus);
+      unirminus = unirminus + codificarminus;
+      //console.log("unir",unir)
+      
+    }
+    return unirminus.toLowerCase();
+  }
+  else{
     var unir = "";
     for(var i = 0;i<string.length;i++){
       var a = string.charCodeAt(i);
@@ -42,7 +78,11 @@ const cipher = {
       
     }
     return unir;
+
   }
+     
+   }
+    
 };
 
 export default cipher;
